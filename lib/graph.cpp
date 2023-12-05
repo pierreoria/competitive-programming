e7938bdda60e764321d5daca7d8fc0ba97acf449
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2023-12-05 14:51:45 -0300

    Create graph.cpp

---
+++ lib/graph.cpp
@@ -0,0 +1,32 @@
+// dijkstra
+typedef long long ll;
+typedef pair<long long, long long> pii;
+
+const ll mx = 1e5;
+ll dist[mx];
+bitset<mx> vis;
+
+void dijkstra(vector<vector<pii>>& g)
+{
+    ll u,v,d,w;
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    d = 0;
+    pq.push({d,0});
+    
+    while(!pq.empty())
+    {
+
+        d = pq.top().ff; u = pq.top().ss; pq.pop();
+        if (vis[u]) continue;
+        vis[u] = true;
+        for (pii p : g[u])
+        {
+            w = p.ff; v = p.ss;
+
+            if (d + w < dist[v]){
+                dist[v] = d + w;
+                pq.push({d+w,v});
+            }
+        }
+    }
+}
