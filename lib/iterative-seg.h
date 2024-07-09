e7d206b5d9bdb62e35ca1faf6f6ec71b788c7216
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2024-07-09 11:02:28 -0300

    Update and rename seg.cpp to iterative-seg.h

---
+++ lib/iterative-seg.h
@@ -0,0 +1,38 @@
+/*
+  low constant factors, 2*n memory iterative seg tree
+  source: AI.Cash https://codeforces.com/blog/entry/18051
+  0-based, [l,r) type queries
+  seg[n] to seg[2n-1] corresponds to array[0] to array[n-1]
+  for N != 2^k, cannot be used as is for efficient binary search, as there will be more than one root 
+                (see https://codeforces.com/contest/1354/submission/80923473)
+*/
+
+const int N = 1e5;  // limit for array size
+int n;  // array size
+int t[2 * N];
+
+void build() {  // build the tree
+  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+}
+
+void modify(int p, int value) {  // set value at position p
+  for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+}
+
+int query(int l, int r) {  // sum on interval [l, r)
+  int res = 0;
+  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+    if (l&1) res += t[l++];
+    if (r&1) res += t[--r];
+  }
+  return res;
+}
+
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; ++i) scanf("%d", t + n + i);
+  build();
+  modify(0, 1);
+  printf("%d\n", query(3, 11));
+  return 0;
+}
