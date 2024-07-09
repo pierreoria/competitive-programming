3a721cbc2dea8a8358cfd08d3b7f89a31ea4d528
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2024-07-09 10:52:34 -0300

    Create range-assignment-seg.h

---
+++ lib/range-assignment-seg.h
@@ -0,0 +1,44 @@
+/*
+ range assignment, lazy propagation seg tree
+ 0-based
+ root = 1 (tl = 0, tr = n-1)
+ closed intervals
+*/
+
+const int mx = 1e5+5;
+int t[4*mx];
+
+
+void push(int v) {
+    if (marked[v]) {
+        t[v*2] = t[v*2+1] = t[v];
+        marked[v*2] = marked[v*2+1] = true;
+        marked[v] = false;
+    }
+}
+
+void update(int v, int tl, int tr, int l, int r, int new_val) {
+    if (l > r) 
+        return;
+    if (l == tl && tr == r) {
+        t[v] = new_val;
+        marked[v] = true;
+    } else {
+        push(v);
+        int tm = (tl + tr) / 2;
+        update(v*2, tl, tm, l, min(r, tm), new_val);
+        update(v*2+1, tm+1, tr, max(l, tm+1), r, new_val);
+    }
+}
+
+int get(int v, int tl, int tr, int pos) {
+    if (tl == tr) {
+        return t[v];
+    }
+    push(v);
+    int tm = (tl + tr) / 2;
+    if (pos <= tm) 
+        return get(v*2, tl, tm, pos);
+    else
+        return get(v*2+1, tm+1, tr, pos);
+}
