dbc8c1aea38c80eb910f2a381ad5dc637f20a8ac
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2024-07-09 10:50:46 -0300

    Create range-update-seg.h

---
+++ lib/range-update-seg.h
@@ -0,0 +1,43 @@
+/*
+ vanilla range update seg, sum point queries
+ 0-based
+ root = 1 (tl = 0, tr = n-1)
+ closed intervals
+*/
+
+const int mx = 1e5+5;
+int t[4*mx];
+
+void build(vector<int>& a, int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v] = a[tl];
+    } else {
+        int tm = (tl + tr) / 2;
+        build(a, v*2, tl, tm);
+        build(a, v*2+1, tm+1, tr);
+        t[v] = 0;
+    }
+}
+
+// range update
+void update(int v, int tl, int tr, int l, int r, int add) {
+    if (l > r)
+        return;
+    if (l == tl && r == tr) {
+        t[v] += add;
+    } else {
+        int tm = (tl + tr) / 2;
+        update(v*2, tl, tm, l, min(r, tm), add);
+        update(v*2+1, tm+1, tr, max(l, tm+1), r, add);
+    }
+}
+
+int get(int v, int tl, int tr, int pos) {
+    if (tl == tr)
+        return t[v];
+    int tm = (tl + tr) / 2;
+    if (pos <= tm)
+        return t[v] + get(v*2, tl, tm, pos);
+    else
+        return t[v] + get(v*2+1, tm+1, tr, pos);
+}
