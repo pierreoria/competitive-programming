54d42853fba45078f0ab0d90e3dad9dffa4b99f4
Pierre Oriá <108680221+pierreoria@users.noreply.github.com>
2024-07-22 17:20:40 -0300

    Update G.cpp

--- gym/G.cpp
+++ gym/G.cpp
@@ -8,195 +8,160 @@ using namespace std;
 #define br '\n'
 
 const int mx = 1e5+5;
-bitset<mx> p;
-
-void gen()
-{
-    p.set();
-    p[0] = p[1] = 0;
-    int lim = sqrt(mx);
-    for (int i = 2; i <= lim; i++)
-    {
-        if (p[i])
-        {
-            int j = i+i;
-            while(j < mx)
-            {
-                p[j] = 0;
-                j += i;
-            }
-        }
-    }
-}
+int FS[mx], GF[mx];
 
 int factorsum(int x)
 {
+    int o = x;
     if (x == 1) return 0;
+    if (FS[x] != -1) return FS[x];
     int lim = sqrt(x);
     int res = 0;
     for (int i = 2; i <= lim && x > 1; i++)
     {
-        if (p[i]){
-            while (!(x%i)){
-                if ((x/i) > lim && p[x/i]) {
-                    res += x/i;
-                    x /= (x/i);
-                    
-                }
-                res += i;
-                x /= i;
-            }
+        while (!(x%i)){
+            res += i;
+            x /= i;
         }
     }
-    return (res ? res : x);
+
+    if (x > 1) res += x;
+
+    return FS[o] = res;
 }
 
 int greatestfactor(int x)
 {
+    int o = x;
     if (x == 1) return 1;
+    if (GF[x] != -1) return GF[x];
     int lim = sqrt(x);
-    // fator entre raiz(x) e x
-    for (int i = 2; i <= lim; i++)
+    int ans = 1;
+    for (int i = 2; i <= lim; i++){
+        while (!(x%i)) ans = max(ans,i), x /= i;
+    }
+    ans = max(ans,x);
+    return GF[o] = ans;
+}
+
+struct segTree{
+    int n;
+    vector<int> val, sum, lazy;
+
+    segTree(int nn)
     {
-        if (!(x%i) && p[x/i]){
-            return x/i;
+        n = nn;
+        val.resize(n<<2);
+        sum.resize(n<<2);
+        lazy.resize(n<<2);
+    }
+
+    void build(vector<int>& a, int node, int l, int r)
+    {
+        if (l == r) {
+            val[node] = a[l];
+            sum[node] = factorsum(a[l]);
+        }
+        else {
+            int m = (l+r)>>1;
+            build(a, node<<1, l, m);
+            build(a, (node<<1)|1, m+1 ,r);
+            val[node] = val[node<<1] + val[(node<<1)|1];
+            sum[node] = sum[node<<1] + sum[(node<<1)|1];
         }
     }
-    int res = 0;
-    // fator entre 2 e raiz x
-    for (int i = 2; i <= lim; i++)
+
+    void update_lazy(int node, int l, int r)
     {
-        if (p[i] && !(x%i)){
-            res = max(res, i);
+        if (!lazy[node]) return;
+        int v = lazy[node];
+        val[node] = v;
+        sum[node] = factorsum(v) * (r-l+1);
+    }
+
+    void push(int node, int l, int r)
+    {
+        if (lazy[node])
+        {
+            lazy[node<<1] = lazy[(node<<1)|1] = lazy[node];
+            update_lazy(node, l, r);
+
+            int m = (l+r)>>1;
+            update_lazy(node<<1, l, m);
+            update_lazy((node<<1)|1, m+1, r);
+            lazy[node] = 0;
         }
     }
-    // se x for primo, retornar x
-    return (res == 0 ? x : res);
-}
 
-struct node {
-    int val,sum,sz;
+    void assign_range(int node, int tl, int tr, int l, int r, int v)
+    {
+        if (l > r) return;
+        if (tl == l && tr == r) {
+            lazy[node] = v;
+            if (tl == tr) update_lazy(node, tl, tr);
+            else push(node, tl, tr);
+        }
+        else {
+            push(node, tl, tr);
+            int tm = (tl + tr)>>1;
+            assign_range(node<<1, tl, tm, l, min(tm,r), v);
+            assign_range((node<<1)|1, tm+1, tr, max(tm+1,l), r, v);
 
-    node(){
-        sz = 1;
+            sum[node] = sum[node<<1] + sum[(node<<1)|1];
+        }
     }
 
-    node(int v){
-        val = v;
-        sum = factorsum(val);
-        sz = 1;
-    }
-};
-
-node t[mx<<2];
-bool marked[mx<<2];
-int n;
-
-node comb(int l, int r)
-{
-    node res;
-
-    res.sz = t[l].sz + t[r].sz;
-    res.val = t[l].val + t[r].val; // armazena valor lixo pra nós internos não marcados
-    res.sum = (marked[l] ? t[l].sum * t[l].sz : t[l].sum) + (marked[r] ? t[r].sum * t[r].sz : t[r].sum);
-
-    return res;
-}
-
-void build(vector<node>& a, int v, int tl, int tr) {
-    if (tl == tr) {
-        t[v] = a[tl];
-        //cout << "1 t[" << v << "].val = " << t[v].val << " t[" << v << "].sum = " << t[v].sum << br;
-    } else {
-        int tm = (tl + tr) / 2;
-        build(a, v*2, tl, tm);
-        build(a, v*2+1, tm+1, tr);
-        t[v] = comb(v<<1, v<<1|1);
-        //cout << "2 t[" << v << "].val = " << t[v].val << " t[" << v << "].sum = " << t[v].sum << br;
-    }
-}
-
-
-void push(int v) {
-    if (marked[v]) {
-        t[v<<1].val = t[v<<1|1].val = t[v].val;
-        t[v<<1].sum = t[v<<1|1].sum = t[v].sum;
-        marked[v<<1] = marked[v<<1|1] = true;
-        marked[v] = false;
-    }
-}
-
-void update(int v, int tl, int tr, int l, int r, int new_val) {
-    if (l > r) 
-        return;
-    if (l == tl && tr == r) {
-        t[v].val = new_val;
-        t[v].sum = factorsum(new_val); // sum * sz pra nós marcados
-        marked[v] = true;
-    } else {
-        push(v);
+    int sumquery(int node, int tl, int tr, int l, int r)
+    {
+        if (l > r) return 0;
+        if (tl == l && tr == r) {
+            if (tl == tr) update_lazy(node, tl, tr);
+            else push(node, tl, tr);
+            return sum[node];
+        }
+        push(node, tl, tr);
         int tm = (tl + tr)>>1;
-        update(v<<1, tl, tm, l, min(r, tm), new_val);
-        update(v<<1|1, tm+1, tr, max(l, tm+1), r, new_val);
-        t[v] = comb(v<<1, v<<1|1);
+        return sumquery(node<<1, tl, tm, l, min(r,tm)) +
+               sumquery((node<<1)|1, tm+1, tr, max(tm+1,l), r);
     }
-}
 
-// range query das somas
-int sumquery(int v, int tl, int tr, int l, int r)
-{
-    if (l > r) return 0;
-    if (tl == l && tr == r) {
-        return (marked[v] ? t[v].sum * t[v].sz : t[v].sum);
+    int valquery(int node, int tl, int tr, int pos)
+    {
+        if (tl > tr) return 0;
+        if (tl == pos && tr == pos){
+            update_lazy(node, tl, tr);
+            return val[node];
+        }
+        push(node, tl, tr);
+        int tm = (tl + tr)>>1;
+        if (pos <= tm)
+            return valquery(node<<1, tl, tm, pos);
+        return valquery((node<<1)|1, tm+1, tr, pos);
     }
-    push(v);
-    int tm = (tl + tr)>>1;
-    return sumquery(v<<1,tl, tm, l, min(r, tm)) + sumquery(v<<1|1, tm+1, tr, max(l,tm+1), r);
-}
 
-// point query valor
-int getval(int v, int tl , int tr, int pos) {
-    if (tl == tr) {
-        return t[v].val;
-    }
-    push(v);
-    int tm = (tl + tr)>>1;
-    if (pos <= tm) 
-        return getval(v<<1, tl, tm, pos);
-    else
-        return getval(v<<1|1, tm+1, tr, pos);
-}
-
-
+};
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    gen();
-
-    int q,x,op,l,r,pos,sum,val;
+    memset(GF,-1,sizeof(GF));
+    memset(FS, -1, sizeof(FS));
+    int n,q,x,op,l,r,pos,sum,val;
 
     cin>>n;
-    vector<node> a(n);
+    vector<int> a(n);
+    
+    segTree seg(n);
+
     for (int i = 0; i < n; i++) {
-        cin>>a[i].val;
-        a[i].sum = factorsum(a[i].val);
-        //cout << "factorsum (" << a[i].val << "): " << a[i].sum << br;
+        cin>>a[i];
     }
 
-    build(a,1,0,n-1);
+    seg.build(a,1,0,n-1);
 
     cin>>q;
-
-    //cout <<"a "<<  factorsum(1) << br;
-    //cout << "q0: " << br;
-    //cout << sumquery(1,0,n-1,0,n-1) << br;
-    for (int i = 0; i < 20; i++){
-        cout << "fat(" << i << "): " << factorsum(i) << br;
-        cout << "greatestfactor(" << i << "): " << greatestfactor(i) << br;
-    }
 
     for (int i = 0 ; i < q; i++)
     {
@@ -204,22 +169,19 @@ int main()
         if (op == 1)
         {
             cin>>pos; pos--;
-            // aqui tem que ser query pq eu n sei se tem ancestral marcado
-            val = getval(1,0,n-1,pos);
-            //cout << "op1: " << val << " /= " << greatestfactor(val) << br;
-            val /= greatestfactor(val);
-            update(1,0,n-1,pos,pos,val);
+            val = seg.valquery(1,0,n-1,pos);
+            if (greatestfactor(val) > 1) val /= greatestfactor(val);
+            seg.assign_range(1,0,n-1,pos,pos,val);
         }
         else if (op == 2)
         {
             cin>>l>>r; l--; r--;
-            //cout << "op2: sumquery(" << l << "," << r << ")" << br;
-            cout << sumquery(1,0,n-1,l,r) << br;
+            cout << seg.sumquery(1,0,n-1,l,r) << endl;
         }
         else
         {
             cin>>l>>r>>x; l--; r--;
-            update(1,0,n-1,l,r,x);
+            seg.assign_range(1,0,n-1,l,r,x);
         }
     }
 
